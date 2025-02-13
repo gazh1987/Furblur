e7c40e98fe8396ab6c1eae1b46ace1002adda366
Gary Healy <gary.healy.87@gmail.com>
2025-02-13 17:41:44 +0000

    reviewed and enhanced map.js focusing on performance, maintainability, readability

--- www/js/map.js
+++ www/js/map.js
@@ -1,3 +1,14 @@
+/**
+ * Some Leaflet Controls have been disabled for clearer UX. 
+ * ZoomControl. This has been removed because the user can simply pinch the screen to zoom in and out.
+ * AttributionControl. This is disabled for clearer UX however, we might add this in if needed later.
+ */
+
+
+// Turn Debug off for Production
+const DEBUG = true;
+var marker = null;
+
 var map = L.map('map',{
     zoomControl: false, // Disable zoom controls
     attributionControl: false, // Disable the attribution control
@@ -6,14 +17,50 @@ var map = L.map('map',{
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
 }).addTo(map);
-map.locate({setView: true, watch: true, maxZoom: 16});
+
+// enableHighAccuracy ensures better accuracy, useful for tracking walking routes in urban areas
+map.locate({
+    setView: true, 
+    watch: true, 
+    maxZoom: 17, 
+    enableHighAccuracy: true } 
+);
 
 function onLocationFound(e) {
-    L.marker(e.latlng).addTo(map);
+    if (DEBUG == true) {
+        console.log("Location found");
+    }
+
+    if (!e.latlng) {
+        console.warn("Invalid Location Received: ", e);
+        return;
+    }
+
+    if (marker) {
+        map.removeLayer(marker);
+    } 
+    marker = L.marker(e.latlng).addTo(map);
+    
 }
 map.on('locationfound', onLocationFound);
 
 function onLocationError(e) {
-    alert(e.message);
+    if (DEBUG == true) {
+        console.log(e.message);
+    }
+    
+    var spanButton = document.getElementById("walkingButton");
+    spanButton.style.pointerEvents = "none";
+    spanButton.id = "walkingButtonOnLocationErrorMessage";
+    spanButton.textContent = e.message;
 }
 map.on('locationerror', onLocationError);
+
+// Before page unload or navigating away, stop tracking to prevent unnecessary processing
+window.addEventListener("beforeunload", function() {
+    if (DEBUG == true) {
+        console.log("map has stopped locating");
+    }
+
+    map.stopLocate();
+})
